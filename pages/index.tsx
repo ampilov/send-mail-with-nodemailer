e27bca73fbe61cf324d904d894f8fe7ced00182f
Anton Ampilov <anton_ampilov@mail.ru>
2022-01-13 18:08:07 +0300

    was corrected hook name in inputs

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,8 +18,8 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-1">
       <h1 className='font-bold text-lg text-slate-700'>Test Form for sending data</h1>
       <input onChange={(e)=>{setName(e.target.value)}} name='name' type="text" className='border-2 border-slate-400 rounded p-4' placeholder='Name' />
-      <input onChange={(e)=>{setName(e.target.value)}} name='email' type="email" className='border-2 border-slate-400 rounded p-4' placeholder='Email' />
-      <input onChange={(e)=>{setName(e.target.value)}} name='message' type="text" className='border-2 border-slate-400 rounded p-4' placeholder='Message' />
+      <input onChange={(e)=>{setEmail(e.target.value)}} name='email' type="email" className='border-2 border-slate-400 rounded p-4' placeholder='Email' />
+      <input onChange={(e)=>{setMessage(e.target.value)}} name='message' type="text" className='border-2 border-slate-400 rounded p-4' placeholder='Message' />
       <button onClick={()=>sending()} disabled={submitted} className='bg-green-400 rounded p-4 font-bold text-white text-xl w-[225px]'>Send</button>
     </div>
   )
