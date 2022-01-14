b8d60d7abb1db33cf1729749b57e229ace7bcc14
Anton Ampilov <anton_ampilov@mail.ru>
2022-01-14 14:38:25 +0300

    was added node nodemailer and settings, login and pass

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,12 @@ import Head from 'next/head'
 import { useState } from 'react'
 import axios from 'axios'
 
+type Data = {
+  name: string
+  email: string
+  message: string
+}
+
 export default function Home() {
 
   const [name, setName] = useState('')
@@ -9,19 +15,36 @@ export default function Home() {
   const [message, setMessage] = useState('')
   const [submitted, setSubmitted] = useState(false)
 
-  function sending () {
+  function sending() {
+    
     setSubmitted(true)
-    console.log({name, email, message});
+
+    // input data object
+    const data: Data = {
+      name: name,
+      email: email,
+      message: message
+    }
+
+    //using axios lib
+    axios.post('/api/contact', data)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
     setSubmitted(false)
   }
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-1">
       <h1 className='font-bold text-lg text-slate-700'>Test Form for sending data</h1>
-      <input onChange={(e)=>{setName(e.target.value)}} name='name' type="text" className='border-2 border-slate-400 rounded p-4' placeholder='Name' />
-      <input onChange={(e)=>{setEmail(e.target.value)}} name='email' type="email" className='border-2 border-slate-400 rounded p-4' placeholder='Email' />
-      <input onChange={(e)=>{setMessage(e.target.value)}} name='message' type="text" className='border-2 border-slate-400 rounded p-4' placeholder='Message' />
-      <button onClick={()=>sending()} disabled={submitted} className='bg-green-400 rounded p-4 font-bold text-white text-xl w-[225px]'>Send</button>
+      <input onChange={(e) => { setName(e.target.value) }} name='name' type="text" className='border-2 border-slate-400 rounded p-4' placeholder='Name' />
+      <input onChange={(e) => { setEmail(e.target.value) }} name='email' type="email" className='border-2 border-slate-400 rounded p-4' placeholder='Email' />
+      <input onChange={(e) => { setMessage(e.target.value) }} name='message' type="text" className='border-2 border-slate-400 rounded p-4' placeholder='Message' />
+      <button onClick={() => sending()} disabled={submitted} className='bg-green-400 rounded p-4 font-bold text-white text-xl w-[225px]'>Send</button>
     </div>
   )
 }
